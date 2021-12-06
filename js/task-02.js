20f1ae17e3b0acde7eae02b2c63ca9fdd04038e9
Vadym <loikfit@gmail.com>
2021-12-07 00:44:35 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+let listEl = document.createElement('li');
+// listEl.textContent = '';
+listEl.classList.add('item');
+// console.log(listEl);
+const navEl = document.querySelector('#ingredients');
+// console.log(navEl);
+navEl.appendChild(listEl);
+
+const list = ingredients.reduce((total, item) => total + `<li>${item}</li>`, '');
+
+navEl.innerHTML = list;
