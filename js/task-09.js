20569c62ed4374c125ea7f0099104d527fc9af52
Vadym <loikfit@gmail.com>
2021-12-08 23:51:53 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const showColorSpan = document.querySelector('.color');
+
+const changeColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+const showColor = () => {
+  showColorSpan.textContent = getRandomHexColor();
+};
+
+changeColorButton.addEventListener('click', changeColor);
+changeColorButton.addEventListener('click', showColor);
