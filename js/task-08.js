7d2fbd8b9afb41cafb243028c7f8d9b642f7a7eb
Vadym <loikfit@gmail.com>
2021-12-08 23:36:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+let message = '';
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  //   console.log(formData);
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  if (mail === '' || password === '') {
+    return window.alert('Please, fill in all the fields!');
+  }
+
+  console.log(formData);
+  form.reset();
+}
